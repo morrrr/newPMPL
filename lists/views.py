a0b1551a3b4742adfb1a50bc3cb267d57f6cef86
morcaw <morcaw@live.com>
2015-09-16 16:02:06 +0700

    Last commit plus add

--- lists/views.py
+++ lists/views.py
@@ -5,6 +5,13 @@ def home_page(request):
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
         return redirect('/')
-
+    
+    if Item.objects.count() == 0 :
+        comment = 'yey, waktunya berlibur'
+    if (Item.objects.count() < 5) and (Item.objects.count() != 0) :
+        comment = 'sibuk tapi santai'
+    if Item.objects.count() >= 5 :
+        comment = 'oh tidak'   
+        
     items = Item.objects.all()
-    return render(request, 'home.html', {'items': items})
+    return render(request, 'home.html', {'items': items, 'comment':comment})
