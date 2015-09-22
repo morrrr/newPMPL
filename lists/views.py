0f5cc7e0844a603bd67cf772a69954c9c0f96289
morcaw <morcaw@live.com>
2015-09-22 17:10:05 +0700

    new URL, view and template to display lists

--- lists/views.py
+++ lists/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import redirect, render
 from lists.models import Item
 
+def view_list(request):
+    items = Item.objects.all()
+    return render(request, 'list.html', {'items': items})
+    
 def home_page(request):
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
-        return redirect('/')
+        return redirect('/lists/the-only-list-in-the-world/')
     
     if Item.objects.count() == 0 :
         comment = 'yey, waktunya berlibur'
@@ -13,5 +17,7 @@ def home_page(request):
     if Item.objects.count() >= 5 :
         comment = 'oh tidak'   
         
-    items = Item.objects.all()
-    return render(request, 'home.html', {'items': items, 'comment':comment})
+    return render(request, 'home.html', {'comment':comment})
+
+
+    
