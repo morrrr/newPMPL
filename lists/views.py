477154137c490be2fc1d520dee4c6b1768a99c36
morrrr <aagustinamora@gmail.com>
2015-11-17 11:29:33 +0700

    Refactor list view to handle new item POSTs

--- lists/views.py
+++ lists/views.py
@@ -4,6 +4,9 @@ from django.core.exceptions import ValidationError
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'], list=list_)
+        return redirect('/lists/%d/' % (list_.id,))
     countList = Item.objects.filter(list_id=list_.id).count()
     if countList == 0 :
         comment = 'yey, waktunya berlibur'
@@ -20,10 +23,10 @@ def home_page(request):
     comment = 'yey, waktunya berlibur'    
     return render(request, 'home.html', {'comment':comment})
 
-def add_item(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect('/lists/%d/' % (list_.id,))
+# def add_item(request, list_id):
+#    list_ = List.objects.get(id=list_id)
+#    Item.objects.create(text=request.POST['item_text'], list=list_)
+#    return redirect('/lists/%d/' % (list_.id,))
 
 def new_list(request):
     list_ = List.objects.create()
